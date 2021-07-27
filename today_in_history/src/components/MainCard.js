1dfe842da22f3f7d33b26a1897be39eb50dcf170
Kyle Price <k.price.la@gmail.com>
2021-07-26 21:39:38 -0400

    add event image to main card

--- today_in_history/src/components/MainCard.js
+++ today_in_history/src/components/MainCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import EventImage from './EventImage';
 import '../styles/MainCard.css';
 
 const MainCard = (props) => {
 
     return (
         <div className="cardContainer">
-
+            <EventImage />
         </div>
     )
 }
