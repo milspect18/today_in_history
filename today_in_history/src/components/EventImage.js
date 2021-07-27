78d50bea350a00bb8994fc751935ab7075c509b0
Kyle Price <k.price.la@gmail.com>
2021-07-27 07:14:43 -0400

    add alt text to img

--- today_in_history/src/components/EventImage.js
+++ today_in_history/src/components/EventImage.js
@@ -3,9 +3,14 @@ import ImgPlaceholder from '../assets/placeholder.svg';
 import '../styles/EventImage.css';
 
 const EventImage = (props) => {
+    const PLACEHOLDER_ALT_TEXT = "Placeholder Image";
+
     return (
         <div className='eventImgContainer'>
-            <img src={props.imgSrc || ImgPlaceholder} />
+            <img
+                src={props.imgSrc || ImgPlaceholder}
+                alt={props.altText || PLACEHOLDER_ALT_TEXT}
+            />
         </div>
     )
 }
