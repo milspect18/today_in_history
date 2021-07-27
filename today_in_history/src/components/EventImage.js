f7f89db1204a3fc5a613051e163e6f614fc8927f
Kyle Price <k.price.la@gmail.com>
2021-07-27 07:36:06 -0400

    if a bad img url is given fallback to placeholder

--- today_in_history/src/components/EventImage.js
+++ today_in_history/src/components/EventImage.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ImgPlaceholder from '../assets/placeholder.svg';
 import '../styles/EventImage.css';
 
 const EventImage = (props) => {
     const PLACEHOLDER_ALT_TEXT = "Placeholder Image";
+    const [imagePath, setImagePath] = useState(null);
+
+    useEffect(() => {
+        setImagePath(props.imgSrc)
+    }, [props.imgSrc])
+
+    const onImgPathError = () => {
+        console.log("Cannot load image at path: ", props.imgSrc);
+        setImagePath(null);
+    }
 
     return (
         <div className='eventImgContainer'>
             <img
-                src={props.imgSrc || ImgPlaceholder}
+                onError={onImgPathError}
+                src={imagePath || ImgPlaceholder}
                 alt={props.altText || PLACEHOLDER_ALT_TEXT}
             />
         </div>
