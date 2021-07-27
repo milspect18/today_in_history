3f26593df058c7e86c894b5850ce96aeae3e92b9
Kyle Price <kyleprice@yahoo.com>
2021-07-26 20:29:47 -0400

    create main card file stub

---
+++ today_in_history/src/components/MainCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/MainCard.css';
+
+const MainCard = (props) => {
+
+    return (
+        <div className="cardContainer">
+
+        </div>
+    )
+}
+
+export default MainCard;
