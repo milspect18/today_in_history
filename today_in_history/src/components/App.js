65e65f5cbf30fd24a9b4db9a47f343cb6bf050ba
Kyle Price <k.price.la@gmail.com>
2021-08-02 20:38:32 -0400

    fetch history fact

--- today_in_history/src/components/App.js
+++ today_in_history/src/components/App.js
@@ -1,14 +1,24 @@
+import React, { useEffect, useState } from 'react';
 import MainCard from './MainCard';
 import EventImage from './EventImage';
 import EventTextContent  from './EventTextContent';
+import { EventDetails, HistoricalData } from '../model/HistoricalEvent';
 import '../styles/App.css';
 
 function App() {
+    const [eventDets, setEventDets] = useState(new EventDetails());
+    const [eventImg, setEventImg] = useState({});
+
+    useEffect(() => {
+        HistoricalData.getRandomEventDetails()
+            .then(setEventDets)
+    }, [setEventDets, setEventImg]);
+
     return (
         <div className="appContainer">
             <MainCard>
-                <EventImage />
-                <EventTextContent />
+                <EventImage imgSrc={eventImg?.src} altText={eventImg?.alt} />
+                <EventTextContent eventDetails={eventDets} />
             </MainCard>
         </div>
     );
